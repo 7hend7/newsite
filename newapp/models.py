74d8ff3d64e13ec705f8d034eb1bdc9b1c4ae53c
hend <7hend7@gmail.com>
2020-07-17 09:33:41 +0300

    contact_us

--- newapp/models.py
+++ newapp/models.py
@@ -10,7 +10,12 @@ from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from taggit.models import Tag, TaggedItemBase
 
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel, FieldRowPanel, MultiFieldPanel
+
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField
+)
 from wagtail.core.fields import StreamField
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page, Orderable
@@ -298,11 +303,36 @@ class DefaultBannerImage(models.Model):
         verbose_name_plural = 'Default image for banner'
 
 
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'ContactPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
 
 
+class ContactPage(AbstractEmailForm):
 
+    #template = "contact_page.html"
+    # This is the default path.
+    # If ignored, Wagtail adds _landing.html to your template name
+    #landing_page_template = "contact_page_landing.html"
 
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
 
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label='Form Fields'),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel("subject"),
+        ], heading="Email Settings"),
+    ]
 
 
 
